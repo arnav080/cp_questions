7da6e550051d1f46ca3358802d3c52b943efa9dc
Arnav Gautam <90126200+arnav080@users.noreply.github.com>
2024-03-03 23:50:53 +0530

    Contains Duplicate

--- 217.py
+++ 217.py
@@ -2,8 +2,7 @@
 # 217. Contains Duplicate :
 # Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 
-# approach: 
-      
+
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums.sort()
