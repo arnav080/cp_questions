8dfc5da7481e476bde8aaca0e61049684c166b87
Arnav Gautam <90126200+arnav080@users.noreply.github.com>
2024-03-03 23:50:24 +0530

    Two Sum

--- 1.py
+++ 1.py
@@ -3,7 +3,6 @@
 # You may assume that each input would have exactly one solution, and you may not use the same element twice.
 # You can return the answer in any order.
 
-# approach 
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
