f911d817f256f81b690d78a3498830def8b97d61
Arnav Gautam <90126200+arnav080@users.noreply.github.com>
2024-03-08 22:56:07 +0530

    Reverse Linked List

---
+++ 206.py
@@ -0,0 +1,18 @@
+'''
+Question: 
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+'''
+
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        newHead = head
+        if head.next:
+            newHead = self.reverseList(head.next)
+            head.next.next = head
+        head.next = None
+
+        return newHead
