eee9ad1b4925793366a34a1b346e555355909d14
Arnav Gautam <90126200+arnav080@users.noreply.github.com>
2024-05-03 23:39:28 +0530

    Find Words Containing Character

---
+++ 2942.py
@@ -0,0 +1,17 @@
+'''
+Question:
+You are given a 0-indexed array of strings words and a character x.
+
+Return an array of indices representing the words that contain the character x.
+
+Note that the returned array may be in any order.
+'''
+
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        n = len(words)
+        ans = []
+        for i, word in enumerate(words):
+            if x in word:
+                ans.append(i)
+        return ans
